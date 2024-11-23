b230f071cf74f9a35a7f8eefca1929ec3ad7c64c
Martin Perignáth <martin.perignath@student.tuke.sk>
2024-11-24 00:34:12 +0100

    feat: whyDidYouRender profiler | [#239]

--- Electron/src/renderer/index.tsx
+++ Electron/src/renderer/index.tsx
@@ -3,6 +3,17 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import initOpenAPIClient from '../swagger/openAPIClientInit';
+import whyDidYouRender from '@welldone-software/why-did-you-render';
+import React from 'react';
+
+// Activation why-did-you-render in development mode
+if (process.env.NODE_ENV === 'development') {
+  whyDidYouRender(React, {
+    trackAllPureComponents: true, // Component tracking
+    logOnDifferentValues: true,
+    include: [/^.*$/],
+  });
+}
 
 initOpenAPIClient();
 const container = document.getElementById('root') as HTMLElement;
